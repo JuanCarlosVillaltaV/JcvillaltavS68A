974c4371857bfa3047d0a27ed7b8d8bdb00a3190
JuanCarlosVillaltaV <jcvillalta2008@hotmail.com>
2024-11-17 04:02:39 -0500

    Insertar,guardar todos los datos

--- JcvillaltavS68A/Views/vEstudiante.xaml.cs
+++ JcvillaltavS68A/Views/vEstudiante.xaml.cs
@@ -6,31 +6,28 @@ namespace JcvillaltavS68A.Views;
 
 public partial class vEstudiante : ContentPage
 {
-    private const string Url = "http://192.168.118.46/uisrael/estudiante.php";
-
+    private const string Url = "http://192.168.100.6/uisrael/estudiante.php";
     private readonly HttpClient cliente = new HttpClient();
     private ObservableCollection<Estudiante> estud;
 
-
-
-
-
     public vEstudiante()
-	{
-		InitializeComponent();
+    {
+        InitializeComponent();
         mostrar();
     }
+
     public async void mostrar()
     {
         var content = await cliente.GetStringAsync(Url);
         List<Estudiante> mostrarEst = JsonConvert.DeserializeObject<List<Estudiante>>(content);
         estud = new ObservableCollection<Estudiante>(mostrarEst);
+        //lvEstudiantes.ItemsSource = estud;
         lvEstudiantes.ItemsSource = estud;
 
     }
 
-    private void btnAgregar_Clicked(object sender, EventArgs e)
+    private void btnInsertar_Clicked(object sender, EventArgs e)
     {
-
+        Navigation.PushAsync(new vInsertar());
     }
 }
